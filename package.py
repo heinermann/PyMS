3d51e4dbfbed8339051b6c2c230095c7f036f06e
Adam Heinermann <aheinerm@gmail.com>
2021-11-03 00:52:23 -0700

    Drastically reduce package filesize using shared libs

--- package.py
+++ package.py
@@ -1,46 +1,32 @@
 import PyInstaller.__main__
-from shutil import copytree, ignore_patterns
+from shutil import copytree, ignore_patterns, rmtree, move
 import os
-import shutil
+from pathlib import Path
 
-def get_icon(name):
-    desiredIcon = f'Images/{name}.ico'
-    return desiredIcon if os.path.exists(desiredIcon) else 'Images/PyMS.ico'
-
-def package(name):
-    print(f'Packaging {name}...')
-
-    PyInstaller.__main__.run([
-        f'{name}.pyw',
-        '--onefile',
-        '--noconsole',
-        '--noconfirm',
-        '--clean',
-        '--log-level=WARN',
-        '--specpath=build',
-        f'--icon=../{get_icon(name)}'
-    ])
+apps = ['PyAI', 'PyBIN', 'PyDAT', 'PyFNT', 'PyGOT', 'PyGRP', 'PyICE', 'PyLO', 'PyMAP', 'PyMPQ', 'PyPAL', 'PyPCX', 'PySPK', 'PyTBL', 'PyTILE', 'PyTRG']
 
 if os.path.exists('dist'):
-    shutil.rmtree('dist')
+    rmtree('dist')
 
-package('PyAI')
-package('PyBIN')
-package('PyDAT')
-package('PyFNT')
-package('PyGOT')
-package('PyGRP')
-package('PyICE')
-package('PyLO')
-package('PyMAP')
-package('PyMPQ')
-package('PyPAL')
-package('PyPCX')
-package('PySPK')
-package('PyTBL')
-package('PyTILE')
-package('PyTRG')
+PyInstaller.__main__.run([
+    'bundle.spec',
+    '--noconfirm',
+    '--clean',
+    '--log-level=WARN'
+])
 
 copytree('Images', 'dist/Images')
 copytree('Palettes', 'dist/Palettes')
 copytree('Libs', 'dist/Libs', ignore=ignore_patterns('*.py', '*.pyc', 'Logs', 'Temp', 'Tests', '__pycache__', 'gapy'))
+
+for app_name in apps:
+    src_dir = f'dist/{app_name}/'
+    copytree(src_dir, 'dist/', dirs_exist_ok=True)
+    rmtree(src_dir)
+
+for f in Path('dist/').glob('*.pyd'):
+    move(f, 'dist/Libs/')
+
+for f in Path('dist/').glob('*.dll'):
+    if f.name.startswith('python'): continue
+    move(f, 'dist/Libs/')
